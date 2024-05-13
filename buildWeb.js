1cb4b0ca4a7a942bb03e7e10c1db85894a28a3fb
xyny <git@xyny.anonaddy.me>
2024-05-13 20:00:27 +0300

    feat: generate html page in buildWeb.js

--- buildWeb.js
+++ buildWeb.js
@@ -12,11 +12,26 @@ fs.mkdirSync(`${buildDir}/modules`);
 
 fs.copyFileSync('./_headers', `${buildDir}/_headers`);
 
+const ids = []
+
 fs.readdirSync(sourceDir).forEach((file) => {
     if (file.endsWith('.json')) {
         const filePath = path.join(sourceDir, file);
         const { $id } = JSON.parse(fs.readFileSync(filePath));
         fs.copyFileSync(filePath, `${buildDir}/${$id}`);
+        ids.push($id)
     }
 });
 
+console.log("Files built: " + ids.join(', '))
+console.log("Generating index.html...")
+
+const template = fs.readFileSync('./template-index.html').toString()
+const indexHtml = template.replace(
+    '%DYNAMIC_SCHEMA_LIST%',
+    ids.map(
+        id => `<li><a href="${id}">${id}</a></li>`
+    ).join('')
+)
+
+fs.writeFileSync(`${buildDir}/index.html`, indexHtml)
